13d3b7ccc8cb091549ea6416b6039df561eb5010
JuliaFee <julia.silva133@aluno.senai.br>
2024-05-06 15:55:14 -0300

    feat specific GET routes

--- index.js
+++ index.js
@@ -28,6 +28,35 @@ app.get('/fighters', async (req, res) => {
     }
 });
 
+// rota GET fighters por id
+app.get('/fighters/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const resultado = await pool.query('SELECT * FROM fighters WHERE id = $1', [id]);
+        res.json({
+            total: resultado.rowCount,
+            fighters: resultado.rows
+        });
+    } catch (error) {
+        console.error('Erro ao obter lutador');
+        res.status(500).send({ mensagem: 'Erro interno ao obter lutador' });
+    }
+});
+
+// rota get fighters por nome
+app.get('/fighters/:f_name', async (req, res) => {
+    try {
+        const { f_name } = req.params;
+        const resultado = await pool.query('SELECT * FROM fighters WHERE f_name = $1', [f_name]);
+        res.json({
+            total: resultado.rowCount,
+            fighters: resultado.rows });
+    } catch (error) {
+        console.error('Erro ao obter lutador');
+        res.status(500).send({ mensagem: 'Erro interno ao obter lutador' });
+    }
+});
+
 // rota POST Fighters
 
 app.post('/fighters', async (req, res) => {
